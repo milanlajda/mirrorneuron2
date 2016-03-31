2fad4d7150853069c0145549173aab112e881ec8
milanlajda <milanlajda1@gmail.com>
2016-03-31 21:15:07 +0100

    just starting out with this

--- JOY.py
+++ JOY.py
@@ -35,6 +35,8 @@ joynetwork.sortModules()
 
 # Dataset - checknut co to vsetko znamena #######################################################
 
+
+# To have a nice dataset for visualization, we produce a set of points in 2D belonging to three different classes.
 means = [(-1,0),(2,4),(3,1)]
 cov = [diag([1,1]), diag([0.5,1.2]), diag([1.5,0.7])]
 alldata = ClassificationDataSet(2, 1, nb_classes=3)
@@ -43,4 +45,14 @@ for n in range(400):
         input = multivariate_normal(means[klass],cov[klass])
         alldata.addSample(input, [klass])
 
+# Randomly split the dataset into 75% training and 25% test data sets.
+# Of course, we could also have created two different datasets to begin with.
 tstdata, trndata = alldata.splitWithProportion( 0.25 )
+
+# For neural network classification, it is highly advisable to encode classes
+# with one output neuron per class. Note that this operation duplicates the original
+# targets and stores them in an (integer) field named ‘class’.
+
+trndata._convertToOneOfMany( )
+tstdata._convertToOneOfMany( )
+
